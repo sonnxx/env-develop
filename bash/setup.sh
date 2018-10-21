89b46ee149184ef3e8b0202f8cc371ecdd966dc2
Nguyen Xuan Son <xuansonitvn@gmail.com>
2018-10-21 23:28:04 +0700

    add config tools db

--- bash/setup.sh
+++ bash/setup.sh
@@ -5,10 +5,14 @@ DIR=$( cd -P "$( dirname "$SOURCE" )" && cd .. && pwd )
 
 cd ${DIR}
 
+if [ ! -d "${DIR}/source/html" ]; then
+	mkdir "${DIR}/source/html"
+fi
+
 docker-compose -f docker-compose.yml down;
 docker-compose -f docker-compose.yml up -d --build;
 
 cd -P "$( dirname "$SOURCE" )"
-sh tests/curl.sh
-sleep 5
+# sh tests/curl.sh
+# sleep 5
 sh tests/docker.sh
