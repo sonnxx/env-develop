49729d8fe7142bdfe31720f9577e51bd62413591
Nguyen Xuan Son <xuansonitvn@gmail.com>
2018-10-30 09:20:53 +0700

    update #!/bin/bash

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 SOURCE="${BASH_SOURCE[0]}"
 DIR=$( cd -P "$( dirname "$SOURCE" )" && cd .. && pwd )
 
-cd ${DIR}
+# cd ${DIR}
 
 if [ ! -d "${DIR}/source/html" ]; then
 	mkdir "${DIR}/source/html"
 fi
 
-dcf=docker-compose.yml
+dockerComposeFile=docker-compose.yml
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
 	Darwin*)
 		machine=MacOS
-		dcf=docker-compose.mac.yml
+		dockerComposeFile=docker-compose.mac.yml
 		;;
 	Linux*) machine=Linux;;
 	CYGWIN*) machine=Cygwin;;
@@ -24,8 +24,8 @@ case "${unameOut}" in
 esac
 echo ${machine}
 
-docker-compose -f ${dcf} down;
-docker-compose -f ${dcf} up -d --build;
+docker-compose -f ${dockerComposeFile} down;
+docker-compose -f ${dockerComposeFile} up -d --build;
 
 cd -P "$( dirname "$SOURCE" )"
 # sh tests/curl.sh
